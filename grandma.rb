afabf6c7a184a490943590f4db8d75925be64a7f
Cherissa Woudenberg <snerzel@comcast.net>
2021-03-25 01:04:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(say)
 elsif say == say.upcase
   "NO, NOT SINCE 1938!"
   else
-    puts "HUH?! SPEAK UP, SONNY!"
+     "HUH?! SPEAK UP, SONNY!"
   end
 end
 # Whatever you say to grandma, she should respond with
