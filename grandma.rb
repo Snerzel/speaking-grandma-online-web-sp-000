49bdaee2e458c6a5333801bf0f686abf1bd36cde
Cherissa Woudenberg <snerzel@comcast.net>
2021-03-25 01:04:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(say)
   if say == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  #elsif say.upcase
-  #  puts "NO, NOT SINCE 1938!"
+  "I LOVE YOU TOO PUMPKIN!"
+elsif say == say.upcase
+  "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
